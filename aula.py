879d5aed734696870cf102f946dac97cde26ec8a
Julio_Santana <juliomcj22@gmail.com>
2020-09-08 11:08:24 -0300

    Soma pronta e subtração

--- aula.py
+++ aula.py
@@ -1,5 +1,10 @@
-print("SOMA")
+print("SOMA OU SUBTRAÇÃO ")
+op = int(input("1-SOMA ou  SUBTRAÇÃO"))
 a = int (input("Digite um valor"))
 b = int (input("Digite outro valor"))
-resultado = a + b
+resultado = 0
+if op == 1:
+     resultado = a + b
+ else:
+     resultado = a - b
 print (resultado)
